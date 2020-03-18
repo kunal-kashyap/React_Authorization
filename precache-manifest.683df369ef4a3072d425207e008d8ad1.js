ba22dcb57c49a2996788916b5bbdd684db3a0e2b
Kunal Kashyap <kunal.kashyap@gitlab.intelligrape.net>
2020-03-18 12:59:07 +0530

    Updates

---
+++ precache-manifest.683df369ef4a3072d425207e008d8ad1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb818e158a9467917b0904ef1f6d435d",
+    "url": "/react_authorization/index.html"
+  },
+  {
+    "revision": "d053ef1df330784f55ed",
+    "url": "/react_authorization/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3cc2d39280e67856dca3",
+    "url": "/react_authorization/static/js/2.0f921ff3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react_authorization/static/js/2.0f921ff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d053ef1df330784f55ed",
+    "url": "/react_authorization/static/js/main.e6d47874.chunk.js"
+  },
+  {
+    "revision": "027956c79976023388be",
+    "url": "/react_authorization/static/js/runtime-main.d3d2fef8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_authorization/static/media/logo.5d5d9eef.svg"
+  }
+]);
